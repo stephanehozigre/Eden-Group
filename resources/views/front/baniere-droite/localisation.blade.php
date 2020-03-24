3c9355e428d457275bf22cdb51a193ef58a3da10
TANOHPARFAIT <parfaittanoh42@gmail.com>
2020-03-24 00:05:01 +0000

    modification fichier banniere droite

--- resources/views/front/baniere-droite/localisation.blade.php
+++ resources/views/front/baniere-droite/localisation.blade.php
@@ -3,52 +3,6 @@
 		<div class="custom-heading style-1">
 	        <h1>Localisation</h1>
 	    </div>
-		<div>
-			<div id="myCarousel" class="image-box clearfix carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-            
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                <div class="item active animated fadeInUp">
-                    <img src="./styles/img/Immobilier/matirial_engineering_01.jpg" alt="Chania">
-                    <div class="carousel-caption">
-                        <h3>Los Angeles</h3>
-                        <p>LA is always so much fun!</p>
-                    </div>
-                </div>
-            
-                <div class="item animated">
-                    <img src="./styles/img/Immobilier/img2.jpg" alt="Chicago">
-                    <div class="carousel-caption">
-                        <h3>Chicago</h3>
-                        <p>Thank you, Chicago!</p>
-                    </div>
-                </div>
-            
-                <div class="item">
-                    <img src="./styles/img/Immobilier/img3.jpg" alt="New York">
-                    <div class="carousel-caption">
-                        <h3>New York</h3>
-                        <p>We love the Big Apple!</p>
-                    </div>
-                </div>
-                </div>
-            
-                <!-- Left and right controls -->
-                <a class="left carousel-control" data-target="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" data-target="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-		</div>
+		<iframe src="https://www.google.com/maps/d/embed?mid=1k5OgI5zJQULUt75LJdTs7JCF7sA&hl=fr" width="305" height="500"></iframe>
 	</div>
 </div>
